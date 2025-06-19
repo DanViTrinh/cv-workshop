741dbe1af8db7908d084d82520ed4c5674c38c4f
DanViTrinh <danvitrinh@outlook.com>
2025-06-19 12:21:24 +0200

    update infrastructure part

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -8,9 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name   = "tfstate"
-    
-    storage_account_name  = "tfstate30619"
-
+    storage_account_name  = "tfstate19841"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
